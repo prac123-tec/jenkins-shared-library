ccfdbf93b7fd17ce76564a4cbfa7c7966d0f7160
prac123-tec <learnaws582@gmail.com>
2026-07-27 13:24:37 +0000

    fixed deploy eof

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -2,26 +2,22 @@ def call() {
 
     sshagent(['deploy-server-ssh']) {
 
-        sh '''
-        ssh -o StrictHostKeyChecking=no ec2-user@13.233.80.41 << 'EOF'
+        sh """
+ssh -o StrictHostKeyChecking=no ec2-user@13.233.80.41 <<EOF
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 405305209495.dkr.ecr.ap-south-1.amazonaws.com
 
-        aws ecr get-login-password --region ap-south-1 | \
-        docker login --username AWS --password-stdin 405305209495.dkr.ecr.ap-south-1.amazonaws.com
+docker pull 405305209495.dkr.ecr.ap-south-1.amazonaws.com/platform-control-service:v1
 
-        docker pull 405305209495.dkr.ecr.ap-south-1.amazonaws.com/platform-control-service:v1
+docker stop platform-control-service || true
 
-        docker stop platform-control-service || true
+docker rm platform-control-service || true
 
-        docker rm platform-control-service || true
-
-        docker run -d \
-            --name platform-control-service \
-            -p 80:80 \
-            405305209495.dkr.ecr.ap-south-1.amazonaws.com/platform-control-service:v1
-
-        EOF
-        '''
-
+docker run -d \
+  --name platform-control-service \
+  -p 80:80 \
+  405305209495.dkr.ecr.ap-south-1.amazonaws.com/platform-control-service:v1
+EOF
+"""
     }
 
 }
