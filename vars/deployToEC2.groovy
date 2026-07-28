c4250054d7c548364b5a5c0aa892852fbddc2e01
prac123-tec <learnaws582@gmail.com>
2026-07-28 11:59:56 +0000

    added client ip

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -3,7 +3,7 @@ def call() {
     sshagent(['deploy-server-ssh']) {
 
         sh '''
-ssh -o StrictHostKeyChecking=no ec2-user@13.201.226.140 <<EOF
+ssh -o StrictHostKeyChecking=no ec2-user@15.252.88.70 <<EOF
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 405305209495.dkr.ecr.ap-south-1.amazonaws.com
 
 docker pull 405305209495.dkr.ecr.ap-south-1.amazonaws.com/platform-control-service:v1
